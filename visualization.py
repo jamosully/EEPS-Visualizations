6876cb44b777fba509d980392cd1a5d64a9a4679
jamosully <gx19928@bristol.ac.uk>
2024-06-13 22:30:30 +0100

    Started work on interface

--- visualization.py
+++ visualization.py
@@ -1,6 +1,8 @@
 import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtWidgets import QCheckBox, QWidget, QGridLayout, QHBoxLayout
+from PySide6.QtWidgets import QMainWindow, QLineEdit, QLabel, QComboBox
 import initialization
 import initialization_detail
 import environment as env
@@ -8,26 +10,50 @@ import agent as agn
 import interaction as intrc
 import pdb
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        """
+        Code sourced from:
+        https://stackoverflow.com/questions/12459811/how-to-embed-matplotlib-in-pyqt
+        """
 
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+        self.setWindowTitle("Affinity.net")
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        control_widget = QWidget(self)
+        control_layout = QGridLayout(control_widget)
+        control_layout.setColumnStretch(1, 1)
 
-        self.button.clicked.connect(self.magic)
+        self.figure = Figure()
+
+        self.canvas = FigureCanvas(self.figure)
+
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        main_widget = QWidget(self)
+        main_layout = QHBoxLayout(main_widget)
+        main_layout.setStretch(0, 1)
+        main_layout.addWidget(control_widget)
+        main_layout.addWidget(self.canvas)
+        # self.setCentralWidget(main_widget)
+
+        # self.button = QtGui.QPushButton('Plot')
+        # self.button.clicked.connect(self.plot_agent_memory)
+
+        # layout = QtGui.QVBoxLayout()
+        # layout.addWidget(self.toolbar)
+        # layout.addWidget(self.canvas)
+        # layout.addWidget(self.button)
+        # self.setLayout(layout)
 
     @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+    def plot_agent_memory(self):
+        return
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
