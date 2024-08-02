151704c3435cb92aa11a50881544d4ba7dbfb37d
jamosully <gx19928@bristol.ac.uk>
2024-08-02 18:05:48 +0100

    Added first parts of parameter config

--- parameter_details.py
+++ parameter_details.py
@@ -1,6 +1,6 @@
 # UI Modules
 from PySide6 import QtCore, QtWidgets 
-from PySide6.QtWidgets import QVBoxLayout, QTableWidget, QComboBox, QLabel, QGroupBox, QSizePolicy, QHeaderView, QPushButton, QHBoxLayout
+from PySide6.QtWidgets import QVBoxLayout, QTableWidget, QComboBox, QLabel, QGroupBox, QSizePolicy, QHeaderView, QPushButton, QHBoxLayout, QCheckBox, QDoubleSpinBox
 
 import EEPS.initialization
 import EEPS.initialization_detail
@@ -60,21 +60,49 @@ class EnvParamTable(QtWidgets.QWidget):
 
     def __init__(self, env_detail, env_params):
         QtWidgets.QWidget.__init__(self)
-        #self.simulator = simulator
 
         self.table = QTableWidget(len(env_params), 2)
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
         self.table.verticalHeader().setVisible(False)
         self.table.horizontalHeader().setVisible(False)
 
-        for i, (param_name, value) in enumerate(env_params.items()):
+        self.env_params = env_params
+
+        for i, (param_name, value) in enumerate(self.env_params.items()):
             param_label = QLabel()
             param_label.setIndent(5)
             param_label.setText(param_name)
-            print(i, param_name, value)
+            self.table.setCellWidget(i, 1, self.createTableWidget(value, param_name))
             self.table.setCellWidget(i, 0, param_label)
 
         self.table.resizeColumnsToContents()
+
+    def createTableWidget(self, value, key):
+
+        # if param_type == bool:
+        #     table_widget = QCheckBox(self)
+        #     table_widget.setObjectName(key)
+        #     table_widget.clicked.connect(lambda: self.adjust_params(table_widget.checkState()))
+        #     return table_widget
+        # elif param_type ==  int:
+        #     if
+
+        match key:
+            case "experiment_ID":
+                return
+            case "environment_ID":
+                return
+            case "max_trial":
+                return
+            case "num_agents":
+                return
+            case "size_action_set":
+                return
+
+    def adjust_params(self, value):
+
+        key = self.sender().objectName()
+        self.env_params[key] = value
 
 class AgentParamTable(QtWidgets.QWidget):
 
@@ -88,16 +116,42 @@ class AgentParamTable(QtWidgets.QWidget):
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
         self.table.verticalHeader().setVisible(False)
         self.table.horizontalHeader().setVisible(False)
+
+        self.agent_params = agent_params
+
         self.generate_descriptions()
 
         for i, (param_name, value) in enumerate(agent_params.items()):
             param_label = QLabel()
             param_label.setIndent(5)
             param_label.setText(param_name)
-            print(i, param_name, value)
+            self.table.setCellWidget(i, 1, self.createTableWidget(value, param_name))
             self.table.setCellWidget(i, 0, param_label)
 
         self.table.resizeColumnsToContents()
+
+    def createTableWidget(self, value, key):
+
+        match key:
+            case "network_enhancement":
+                table_widget = QCheckBox(self)
+                table_widget.setObjectName(key)
+                table_widget.clicked.connect(lambda: self.adjust_params(key, table_widget.checkState()))
+                return table_widget
+            case "beta_h" | "K" | "gamma_damping" | "alpha":
+                table_widget = QDoubleSpinBox(self)
+                table_widget.setMaximum(1.00)
+                table_widget.setMinimum(0.00)
+                table_widget.setStepType(QDoubleSpinBox.StepType.AdaptiveDecimalStepType)
+                table_widget.setDecimals(3)
+                return table_widget
+            case "beta_t":
+                return
+            
+    def adjust_params(self, key, value):
+
+        self.agent_params[key] = value
+
 
     def generate_descriptions(self):
 
